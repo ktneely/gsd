dc4e90530cb6fd2364a929fd0d5a949597f57b5b
Kevin T. Neely <ktneely@astroturfgarden.com>
2013-04-08 09:40:16 -0700

    formatting updates and some unnecessary line removal

--- status.sh
+++ status.sh
@@ -7,35 +7,49 @@ DATE=`date +%Y-%m-%d`
 REPORT=status-$DATE
 #alias t='~ktneely/bin/todo'
 
-echo "{ Legend" > $REPORT
-echo "{ The todos loosely follow the GTD methodology" >> $REPORT
-echo "{ + associates a task or item with a project" >> $REPORT
-echo "{ @ associates a task or item with a context" >> $REPORT
-echo "{" >> $REPORT
-echo " "  >> $REPORT
-echo "Status report for week ending $DATE" >> $REPORT
+echo "Status report for week ending $DATE" > $REPORT
 echo "==================================="  >> $REPORT
 echo " " >> $REPORT
 echo "Actions needed:"  >> $REPORT
 echo "--------------- " >> $REPORT
-~ktneely/bin/todo.txt-cli/todo.sh -p list @waiting >> $REPORT
+/home/ktneely/bin/todo.txt-cli/todo.sh -p list @waiting >> $REPORT
 
+echo " " >> $REPORT
+echo " " >> $REPORT
 echo "Staff discussion or topic suggestions:"  >> $REPORT
 echo "--------------------------------------" >> $REPORT
 ~ktneely/bin/todo.txt-cli/todo.sh -p list +@topic >> $REPORT
 
+echo " " >> $REPORT
+echo " " >> $REPORT
 echo "Accomplishments:" >> $REPORT
 echo "----------------" >> $REPORT
 ~ktneely/bin/todo.txt-cli/todo.sh -p lately >> $REPORT
 
+echo " " >> $REPORT
+echo " " >> $REPORT
 echo " "  >> $REPORT 
 echo "Open Todo items:" >> $REPORT
 echo "----------------" >> $REPORT
 ~ktneely/bin/todo.txt-cli/todo.sh -p projectview -@waiting >> $REPORT
 
-# echo "General FYI:" >> $REPORT
-# echo "-------------------" >> $REPORTlately
+echo "General FYI:" >> $REPORT
+echo "-------------------" >> $REPORT
+echo "none at this time" >> $REPORT
 
+echo " " >> $REPORT
+echo " " >> $REPORT
 echo "Upcoming PTO & Training:" >> $REPORT
 echo "-------------------"  >> $REPORT
-remind ~/box/Documents/reminders/travel.txt >> $REPORT
+remind ~/box/Documents/reminders/travel.txt |grep -v Reminders >> $REPORT
+
+echo " " >> $REPORT
+echo " " >> $REPORT
+echo "{ Legend" >> $REPORT
+echo "{ The todos loosely follow the GTD methodology" >> $REPORT
+echo "{ + associates a task or item with a project" >> $REPORT
+echo "{ @ associates a task or item with a context" >> $REPORT
+echo "{ an (A) or (B) indicates priority" >> $REPORT
+echo "{ the number is simply the task number and has no meaning " >> $REPORT
+echo "{ " >> $REPORT
+echo " "  >> $REPORT
